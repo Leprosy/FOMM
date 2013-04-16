103e89a41b8b287523267eef680a9499c6cd86d3
mrojas <leprosy57@gmail.com>
2013-04-16 15:37:18 -0300

    Major cleanup & class re-organization

--- main.py
+++ main.py
@@ -1,10 +1,108 @@
+import pyglet
 import json
-from Game.base import Game
+from pyglet.window import mouse, key
 
+
+class Game(pyglet.window.Window):
+    '''
+    Main definitions and elements for the game
+    '''
+    def __init__(self, cfg):
+        super(Game, self).__init__(800, 600, caption=cfg['title'],
+                                   vsync=False)
+
+        #Environment setup
+        self.cfg = cfg
+        pyglet.resource.path = [self.cfg['game_res']]
+        pyglet.resource.reindex()
+
+        #Global attributes
+        self.inGame = False
+        self.map = None
+        self.party = {"x": 0, "y": 0}
+
+    @staticmethod
+    def getcfg(self):
+        return self.cfg
+
+    def init_game(self):
+        #Load or start a new one?
+        #self.map = Map(self.config_data['game_res'] + '/maps/map1.json')
+
+        #Start
+        self.inGame = True
+
+    def draw_gameGUI(self):
+        #self.clock.draw()
+        pass
+
+    def draw_mainmenu(self):
+        pyglet.text.Label("FOMM",
+                          font_name='Arial',
+                          font_size=32,
+                          x=100, y=300,
+                          anchor_x='left', anchor_y='center').draw()
+        pyglet.text.Label("< Press Spacebar to start >",
+                          font_name='Arial',
+                          font_size=14,
+                          x=100, y=250,
+                          anchor_x='left', anchor_y='center').draw()
+        pyglet.text.Label("< Press ESC to exit >",
+                          font_name='Arial',
+                          font_size=14,
+                          x=100, y=230,
+                          anchor_x='left', anchor_y='center').draw()
+
+    def update(self, secs):
+        #Clear window
+        self.clear()
+
+        if self.inGame:
+            #Game logic
+
+            #Map render
+            self.map.render()
+            pass
+        else:
+            #Main menu
+            self.draw_mainmenu()
+
+    '''
+    Event handlers
+    '''
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.x = x
+        self.y = y
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        #In game mouse clicks
+        if self.inGame:
+            #Game logic
+            pass
+
+    def on_key_press(self, symbol, mod):
+        #Main menu keys
+        if self.inGame is False:
+            if symbol == key.SPACE:
+                self.init_game()
+            if symbol == key.ESCAPE:
+                quit()
+
+        #In game keys
+        else:
+            #Game logic
+            pass
+
+    #Starts
+    def game_start(self):
+        pyglet.clock.schedule_interval(self.update, 1 / 60.0)
+        pyglet.app.run()
 
 ##check events -> window.push_handlers(pyglet.window.event.WindowEventLogger())
 
-#Main entry
+'''
+Begin the game...
+'''
 if __name__ == '__main__':
     config = json.load(open('config.json'))
     G = Game(config)
---
+++ map.py
@@ -0,0 +1,55 @@
+import pyglet
+import math
+import time
+import json
+
+
+class Map:
+    def __init__(self, filename):
+        data = open(filename).read()
+        data = json.loads(data)
+        self.name = data["name"]
+        self.tiles = []
+
+        print Game.getconfig()
+
+        #Definitions
+        self.defs = json.loads(open())
+
+        for i in range(0, len(data["floor"])):
+            aux = []
+
+            for j in range(0, len(data["floor"][i])):
+                aux.append(Tile(data["floor"][i][j], data["thing"][i][j]))
+
+            self.tiles.append(aux)
+
+    def render(self):
+        size = 64
+
+        for i in range(0, len(self.tiles)):
+            for j in range(0, len(self.tiles[i])):
+                pyglet.text.Label("<%s>" % self.tiles[i][j].thing,
+                          font_name="Arial",
+                          font_size=8,
+                          x=j * size, y=i * size,
+                          anchor_x="center", anchor_y="center").draw()
+
+                b = self.tiles[i][j].floor
+                pyglet.graphics.draw(4, pyglet.gl.GL_POLYGON,
+                                    ('v2i', (j * size, i * size,
+                                    (j + 1) * size, i * size,
+                                    (j + 1) * size, (i + 1) * size,
+                                    j * size, (i + 1) * size)),
+                                    ('c3B', (
+                                    60 * b, 200, 10,
+                                    60 * b, 200, 10,
+                                    60 * b, 200, 10,
+                                    60 * b, 200, 10))
+                                    )
+
+
+class Tile:
+    def __init__(self, floor, thing):
+        self.floor = floor
+        self.thing = thing
